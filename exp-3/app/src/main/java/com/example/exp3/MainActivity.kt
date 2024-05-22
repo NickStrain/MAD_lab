774acc5808c9ff14276ca50f8b622ef193ce80bc
unknown <shugavaneshwar13@gmail.com>
2024-05-22 18:06:21 +0530

    up

--- exp-3/app/src/main/java/com/example/exp3/MainActivity.kt
+++ exp-3/app/src/main/java/com/example/exp3/MainActivity.kt
@@ -1,5 +1,4 @@
-package com.example.exp3
-
+package com.example.gra
 import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.Canvas
@@ -13,37 +12,18 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Creating a Bitmap
         val bg = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888)
 
-        // Setting the Bitmap as background for the ImageView
         val i = findViewById<ImageView>(R.id.imageView)
         i.setBackgroundDrawable(BitmapDrawable(resources, bg))
-
-        // Creating the Canvas Object
         val canvas = Canvas(bg)
-
-        // Creating the Paint Object and set its color & TextSize
-        val paint = Paint().apply {
-            color = Color.BLUE
+        val paint = Paint().apply { color = Color.BLUE
             textSize = 50f
         }
-
-        // To draw a Rectangle
-        canvas.drawText("Rectangle", 420f, 150f, paint)
-        canvas.drawRect(400f, 200f, 650f, 700f, paint)
-
-        // To draw a Circle
-        canvas.drawText("Circle", 120f, 150f, paint)
+        canvas.drawText("Circle", 150f, 150f, paint)
         canvas.drawCircle(200f, 350f, 150f, paint)
+        canvas.drawText("ellipse", 120f, 800f, paint)
+        canvas.drawOval(100f,900f,700f,1100f,paint)
 
-        // To draw a Square
-        canvas.drawText("Square", 120f, 800f, paint)
-        canvas.drawRect(50f, 850f, 350f, 1150f, paint)
-
-        // To draw a Line
-        canvas.drawText("Line", 480f, 800f, paint)
-        canvas.drawLine(520f, 850f, 520f, 1150f, paint)
     }
 }
